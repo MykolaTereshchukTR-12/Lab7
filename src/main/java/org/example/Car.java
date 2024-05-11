362414d9ab38bcdde777e61afb74653d685a9fd7
Mykola <nikter387@gmail.com>
2024-05-11 15:19:36 +0300

    Commit 3. Creating a class Driver with fields describing the car: name, age and drivingExperience. Also added methods to the Car class.

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -8,6 +8,52 @@ public class Car {
     private int price;
     private int releaseDate;
 
+    public Car(String model, int enginePower, String driverName, int price, int releaseDate) {
+        this.model = model;
+        this.enginePower = enginePower;
+        this.driverName = driverName;
+        this.price = price;
+        this.releaseDate = releaseDate;
+    }
 
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getEnginePower() {
+        return enginePower;
+    }
+
+    public void setEnginePower(int enginePower) {
+        this.enginePower = enginePower;
+    }
+
+    public String getDriverName() {
+        return driverName;
+    }
+
+    public void setDriverName(String driverName) {
+        this.driverName = driverName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(int releaseDate) {
+        this.releaseDate = releaseDate;
+    }
 
 }
