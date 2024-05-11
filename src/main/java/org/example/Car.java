3753806c2a4e21e3702a77dfdbb90983c8461269
Mykola <nikter387@gmail.com>
2024-05-11 15:12:57 +0300

    Commit 2. Creating a class Car with fields describing the car: model, engine power, driver's name, price, year of release.

---
+++ src/main/java/org/example/Car.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Car {
+
+    private String model;
+    private int enginePower;
+    private String driverName;
+    private int price;
+    private int releaseDate;
+
+
+
+}
